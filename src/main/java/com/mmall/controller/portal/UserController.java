18bfb7fd0d3952cef0141d4e5d9ad5dcb161f697
zhanghao <1226526420@qq.com>
2020-03-29 01:14:58 +0800

    第一个接口写完

---
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -0,0 +1,34 @@
+package com.mmall.controller.portal;
+
+import com.mmall.common.Const;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author zhanghao
+ */
+@Controller
+@RequestMapping("/user/")
+public class UserController {
+
+    @Autowired
+    private IUserService iUserService;
+
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public ServerResponse<User> login(String username, String password, HttpSession session) {
+        System.out.println("12334");
+        ServerResponse<User> response = iUserService.login(username, password);
+        if (response.isSuccess()) {
+            session.setAttribute(Const.CURRENT_USER, response.getData());
+        }
+        return response;
+    }
+
+}
